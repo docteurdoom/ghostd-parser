252fac1125c15c3e2ec9a5623174b2dd74a89a00
docteurdoom <docteurdoom@bitmessage.de>
2023-11-07 08:37:37 -0700

    Add more inactive pools to the list.

--- src/pools.rs
+++ src/pools.rs
@@ -61,8 +61,33 @@ const COLDSTAKE_IO: PrivatePool = PrivatePool {
     url: "https://ghost.coldstake.io/",
     is_active: false,
 };
+const DARKPAY: PrivatePool = PrivatePool {
+    pubkey: "gcs1wmr49e2fd8p09dll8djdfsek6m5zuxhjdq3c4c",
+    url: "https://ghost-pool.darkpay.market/",
+    is_active: false,
+};
+const GHOST_STAKE: PrivatePool = PrivatePool {
+    pubkey: "gcs1cxgy3uzz05djx5j0g40trxkweq7jwllkzzpkf5",
+    url: "https://ghoststake.com/",
+    is_active: false,
+};
+const NOVASTERA_EU: PrivatePool = PrivatePool {
+    pubkey: "gcs1gujv3jjfec75pqkezjckskxw9cfvvdmjkw5g6x",
+    url: "https://ghost.novastera.eu/",
+    is_active: false,
+};
+const GHOST_CHEAP: PrivatePool = PrivatePool {
+    pubkey: "gcs10hnqtq2uvm538n8xck5e7phyqgast5v2m5wv7f",
+    url: "https://ghost.cheap/",
+    is_active: false,
+};
+const GHOST_POOL_DOT_NET: PrivatePool = PrivatePool {
+    pubkey: "gcs154p3rm80dg3arc5jak3293mkgk45uhfggej3da",
+    url: "https://ghost-pool.net/",
+    is_active: false,
+};
 
-pub const POOLS: [PrivatePool; 7] = [
+pub const POOLS: [PrivatePool; 12] = [
     MYGHOST,
     MEGA,
     GHOSTCSP,
@@ -70,4 +95,9 @@ pub const POOLS: [PrivatePool; 7] = [
     POOLGHOSTRUS,
     GHOSTAKE,
     COLDSTAKE_IO,
+    DARKPAY,
+    GHOST_STAKE,
+    NOVASTERA_EU,
+    GHOST_CHEAP,
+    GHOST_POOL_DOT_NET,
 ];
